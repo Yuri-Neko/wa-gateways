ab0cab422fb81618baf40d8c1592210602616fbe
Kyouka Hashiba <91820453+Yuri-Neko@users.noreply.github.com>
2024-03-28 00:14:39 +0700

    Update message_router.js

--- app/routers/message_router.js
+++ app/routers/message_router.js
@@ -3,11 +3,13 @@ const {
   sendMessage,
   sendBulkMessage,
   sendPdf,
+  sendImg,
 } = require("../controllers/message_controller");
 const MessageRouter = Router();
 
 MessageRouter.all("/send-message", sendMessage);
 MessageRouter.all("/send-bulk-message", sendBulkMessage);
 MessageRouter.all("/send-pdf", sendPdf);
+MessageRouter.all("/send-images", sendImg);
 
 module.exports = MessageRouter;
